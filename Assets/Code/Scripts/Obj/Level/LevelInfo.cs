869c37f8aca1a103feed3bdffbdc5541f9875020
= <cjmoshy12@gmail.com>
2025-01-25 11:30:31 -0800

    initial refactoring of code base.

---
+++ Assets/Code/Scripts/Obj/Level/LevelInfo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+[Serializable]
+public class LevelInfo : LevelCore
+{
+    public List<SubLevel> SubLevels = new List<SubLevel>();
+
+    public LevelInfo(string sceneName)
+    {
+        SceneName = sceneName;
+    }
+}
