7db31b41c6b6154976fbc5cdf253aae28af43015
lmurayam <lmurayam@ucsc.edu>
2025-02-16 16:22:30 -0800

    fixed  bug in event system where i instead of id, can no longer click on beaten sublevels

--- Assets/Code/Scripts/Obj/Camera/CameraController.cs
+++ Assets/Code/Scripts/Obj/Camera/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -47,8 +48,16 @@ public class CameraController : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(pointOne2D, Camera.main.transform.forward);
             if (hit.collider != null)
             {
-                if (hit.transform.gameObject.CompareTag("thumbnail"))
+                LevelInfo levelInfo = LevelManager.Instance.Levels[
+                    LevelManager.Instance.currentLevelID
+                ];
+                Debug.Log(hit.collider.gameObject.name);
+                SubLevelInfo sublevel = levelInfo.SubLevels.FirstOrDefault(sublevel =>
+                    sublevel.Thumbnail == hit.collider.gameObject
+                );
+                if (sublevel != null && sublevel.IsComplete != true)
                 {
+                    Debug.Log(sublevel.SceneName);
                     Vector2 levelPosition = hit.transform.position;
                     StartCoroutine(ZoomIn(levelPosition));
                 }
