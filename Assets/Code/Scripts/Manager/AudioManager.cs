ccee05ead093ce4112cf8eab7c3c4aa85a95d412
Elton Zeng <poptako9@gmail.com>
2025-03-07 00:35:40 -0800

    Current bug with the audiomanager where the audio is not playing when the game is started. The audio is only playing when the scene changes.

--- Assets/Code/Scripts/Manager/AudioManager.cs
+++ Assets/Code/Scripts/Manager/AudioManager.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Audio emitter class
 public class AudioManager : MonoBehaviour
 {
     public List<AudioClip> globalSounds = new List<AudioClip>();
     private List<AudioSource> asrs;
+    public AudioClip[] bgm;
+    public AudioSource bgmAudio;
 
     #region Singleton
     private static AudioManager _instance;
@@ -29,6 +32,7 @@ public class AudioManager : MonoBehaviour
         }
 
         asrs = new List<AudioSource>();
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
     #endregion
 
@@ -61,6 +65,35 @@ public class AudioManager : MonoBehaviour
         else
         {
             asrs[i].Play();
+        }
+    }
+
+    //code take from https://discussions.unity.com/t/how-to-have-different-background-music-through-different-scenes/245897
+    void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+    {
+        // Replacement variable (doesn't change the original audio source)
+        AudioSource source = gameObject.AddComponent<AudioSource>();
+
+        // Plays different music in different scenes
+        switch (scene.name)
+        {
+            case "Menu":
+                source.clip = bgm[0];
+                break;
+            case "TestLevel":
+                source.clip = bgm[1];
+                break;
+            default:
+                source.clip = bgm[0];
+                break;
+        }
+
+        // Only switch the music if it changed
+        if (source.clip != bgmAudio.clip)
+        {
+            bgmAudio.enabled = false;
+            bgmAudio.clip = source.clip;
+            bgmAudio.enabled = true;
         }
     }
 }
