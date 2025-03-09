8ea0843d69701addd20d0a603c0aaeee30057b47
Hachristo <bawsomer@gmail.com>
2025-03-09 16:58:30 -0700

    pig follows phone tilt

--- Assets/Code/Scripts/Obj/Shape/ShapeEffects/EffectRawTilt.cs
+++ Assets/Code/Scripts/Obj/Shape/ShapeEffects/EffectRawTilt.cs
@@ -18,7 +18,12 @@ public class EffectRawTilt : MonoBehaviour
         if (cutsceneEnd)
         {
             tilt = eventInfo.VectorOne;
-            float rotationAmount = tilt.x * 180;
+            float rotationAmount = tilt.x * -90.0f;
+            if (tilt.y > 0.0f)
+            {
+                rotationAmount = 180.0f - rotationAmount;
+            }
+            Debug.Log(tilt.x + " " + tilt.y);
             transform.rotation = Quaternion.Euler(0, 0, -rotationAmount);
         }
     }
