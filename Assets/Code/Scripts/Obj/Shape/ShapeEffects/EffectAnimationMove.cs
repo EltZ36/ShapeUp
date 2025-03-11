9682fa214865de24a946e8dde29b631eccc318bd
lmurayam <lmurayam@ucsc.edu>
2025-03-10 19:48:49 -0700

    Changed star settings for different scenes, removed using editor in non editor script

--- Assets/Code/Scripts/Obj/Shape/ShapeEffects/EffectAnimationMove.cs
+++ Assets/Code/Scripts/Obj/Shape/ShapeEffects/EffectAnimationMove.cs
@@ -1,7 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.ProjectWindowCallback;
 using UnityEngine;
 
 public class EffectAnimationMove : MonoBehaviour
