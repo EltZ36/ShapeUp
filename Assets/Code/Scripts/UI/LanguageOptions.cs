c3dcb40ae14d5481a9097f16c53d6881bdf542ba
Nick Corfmat <ncorfmat@ucsc.edu>
2025-04-19 18:17:20 -0700

    Play sound on button click + UI improvements

--- Assets/Code/Scripts/UI/LanguageOptions.cs
+++ Assets/Code/Scripts/UI/LanguageOptions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
-using static UnityEditor.Progress;
 
 public class LanguageOptions : MonoBehaviour
 {
