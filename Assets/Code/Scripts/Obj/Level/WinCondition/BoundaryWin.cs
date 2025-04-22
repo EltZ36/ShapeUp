ee05ac798cbd7755c7a6e8fb4ee83361baa57b5f
Nick Corfmat <ncorfmat@ucsc.edu>
2025-04-21 19:15:52 -0700

    Added pan bounce effect + created sub level win condition base class

--- Assets/Code/Scripts/Obj/Level/WinCondition/BoundaryWin.cs
+++ Assets/Code/Scripts/Obj/Level/WinCondition/BoundaryWin.cs
@@ -1,53 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class BoundaryWin : MonoBehaviour
+public class BoundaryWin : SubLevelWin
 {
-    // Start is called before the first frame update
-    [SerializeField]
-    Shape shapeTarget;
-
-    [SerializeField]
-    GameObject winEffect1,
-        winEffect2;
-
-    string targetName;
-
-    void Awake()
-    {
-        targetName = shapeTarget.ShapeName;
-    }
-
     void OnTriggerExit2D(Collider2D collision)
     {
-        Shape shape = collision.gameObject.GetComponent<Shape>();
-        if (shape != null)
-        {
-            if (shape.ShapeName == targetName)
-            {
-                PlayFireworks(shape.ShapeName);
-                StartCoroutine(CameraController.ZoomOut(false));
-                Destroy(shape.gameObject);
-                Physics2D.gravity = new UnityEngine.Vector2(0f, -9.8f);
-                LevelManager.Instance.OnCurrentSubLevelComplete();
-            }
-        }
-        Destroy(collision.gameObject);
-    }
-
-    void PlayFireworks(string _shape)
-    {
-        winEffect1.SetActive(true);
-        winEffect2.SetActive(true);
-
-        // Animator anim1 = winEffect1.GetComponent<Animator>();
-        // Animator anim2 = winEffect2.GetComponent<Animator>();
-
-        // anim1.Play("Base Layer." + _shape + "Win");
-        // anim2.Play("Base Layer." + _shape + "Win");
+        OnWin(collision.gameObject);
     }
 }
