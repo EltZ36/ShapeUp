847e9852811668d7669b4e1d03a1ac9992756b2a
Nick Corfmat <ncorfmat@ucsc.edu>
2025-04-24 23:29:13 -0700

    resolving runtime debug logs

--- Assets/Code/Scripts/UI/AnchorGameObject.cs
+++ Assets/Code/Scripts/UI/AnchorGameObject.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 // Code modified from Awesometuts,
 // https://awesometuts.com/blog/support-mobile-screen-sizes-unity/?utm_medium=video&utm_source=youtube&utm_campaign=how_to_make_your_game_look_the_same_on_all_mobile_screen_sizes
 
-[ExecuteAlways]
 public class AnchorGameObject : MonoBehaviour
 {
     public enum AnchorType
@@ -41,10 +40,10 @@ public class AnchorGameObject : MonoBehaviour
 #if UNITY_EDITOR
         if (cameraWaitCycles > 0)
         {
-            Debug.Log(
-                $"AnchorGameObject waited {cameraWaitCycles} frame(s) for CameraViewportHandler.Instance. "
-                    + "Consider adjusting script execution order if needed."
-            );
+            //Debug.Log(
+            //    $"AnchorGameObject waited {cameraWaitCycles} frame(s) for CameraViewportHandler.Instance. "
+            //        + "Consider adjusting script execution order if needed."
+            //);
         }
 #endif
         UpdateAnchor();
--- Assets/Code/Scripts/UI/CameraViewportHandler.cs
+++ Assets/Code/Scripts/UI/CameraViewportHandler.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 // Code modified from Awesometuts,
 // https://awesometuts.com/blog/support-mobile-screen-sizes-unity/?utm_medium=video&utm_source=youtube&utm_campaign=how_to_make_your_game_look_the_same_on_all_mobile_screen_sizes
 
-[ExecuteAlways]
 [RequireComponent(typeof(Camera))]
 public class CameraViewportHandler : MonoBehaviour
 {
