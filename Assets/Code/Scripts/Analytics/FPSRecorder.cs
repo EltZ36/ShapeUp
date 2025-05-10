6199efcfa18d88d45fb5e096ccbca84096cad5e8
Nick Corfmat <ncorfmat@ucsc.edu>
2025-05-10 14:14:39 -0700

    added reporting for avg fps and avg low fps

--- Assets/Code/Scripts/Analytics/FPSRecorder.cs
+++ Assets/Code/Scripts/Analytics/FPSRecorder.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Services.Analytics;
 using UnityEngine;
 
@@ -11,9 +12,9 @@ using UnityEngine;
 public class FPSRecorder : MonoBehaviour
 {
     [SerializeField]
-    private float updateInterval = 5f;
+    private float snapshotInterval = 10f;
 
-    public static int CurrentFPS { get; private set; }
+    private List<int> fpsSamples = new List<int>();
 
     #region Singleton Pattern
     private static FPSRecorder _instance;
@@ -40,22 +41,49 @@ public class FPSRecorder : MonoBehaviour
     public void StartTackingFPS()
     {
         StartCoroutine(CalculateFPS());
+        StartCoroutine(TakeFPSSnapshot());
     }
 
     private IEnumerator CalculateFPS()
     {
         while (true)
         {
-            CurrentFPS = Mathf.FloorToInt(1f / Time.deltaTime);
-            RecordFPS();
+            int fps = Mathf.FloorToInt(1f / Time.unscaledDeltaTime);
+            fpsSamples.Add(fps);
 
-            yield return new WaitForSeconds(updateInterval);
+            yield return null;
         }
     }
 
-    public void RecordFPS()
+    private IEnumerator TakeFPSSnapshot()
     {
-        FPSReportEvent fpsReportEvent = new FPSReportEvent { FPS = CurrentFPS };
+        while (true)
+        {
+            yield return new WaitForSeconds(snapshotInterval);
+
+            RecordFPS();
+            fpsSamples.Clear();
+        }
+    }
+
+    private void RecordFPS()
+    {
+        if (fpsSamples.Count == 0)
+            return;
+
+        int averageFPS = Mathf.FloorToInt((float)fpsSamples.Average());
+
+        List<int> sortedFPS = fpsSamples.OrderBy(f => f).ToList();
+
+        int onePercentCount = Mathf.Max(1, Mathf.FloorToInt(sortedFPS.Count * 0.01f));
+        int lowFPSAverage = Mathf.FloorToInt((float)sortedFPS.Take(onePercentCount).Average());
+
+        FPSReportEvent fpsReportEvent = new FPSReportEvent
+        {
+            AverageFPS = averageFPS,
+            AverageOnePercentLowFPS = lowFPSAverage,
+        };
+
         AnalyticsService.Instance.RecordEvent(fpsReportEvent);
     }
 
@@ -64,9 +92,14 @@ public class FPSRecorder : MonoBehaviour
         public FPSReportEvent()
             : base("fpsReport") { }
 
-        public int FPS
+        public int AverageFPS
         {
-            set { SetParameter("fps", value); }
+            set { SetParameter("averageFPS", value); }
+        }
+
+        public int AverageOnePercentLowFPS
+        {
+            set { SetParameter("averageOnePercentLowFPS", value); }
         }
     }
 }
