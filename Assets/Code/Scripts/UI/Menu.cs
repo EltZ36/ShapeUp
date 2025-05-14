e3b8f105f1667fdde5171a3027470bb3312d889a
Nick Corfmat <ncorfmat@ucsc.edu>
2025-05-14 15:37:43 -0700

    adding back buttons + adding thumbnails to level select

--- Assets/Code/Scripts/UI/Menu.cs
+++ Assets/Code/Scripts/UI/Menu.cs
@@ -12,8 +12,6 @@ public class Menu : MonoBehaviour
 
     public void OnDailyPuzzleButton()
     {
-        // redirect to daily puzzle
-        // LevelManager.Instance.LoadLevel(3);
         SceneManager.LoadScene("DailyPuzzle");
     }
 
