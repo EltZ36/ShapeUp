affa2d69ef96a6224134f47352485f120a83983b
Nick Corfmat <ncorfmat@ucsc.edu>
2025-05-17 14:42:09 -0700

    added save localization settings between sessions

--- Assets/Code/Scripts/UI/Menu.cs
+++ Assets/Code/Scripts/UI/Menu.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("LocaleID"))
+        {
+            int localeID = PlayerPrefs.GetInt("LocaleID");
+
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[
+                localeID
+            ];
+        }
+    }
+
     public void OnLevelSelectButton()
     {
         SceneManager.LoadScene("LevelSelect");
