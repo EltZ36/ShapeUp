cc95c0a13c81dac71180c8961f7f228129bd2fda
Elton Zeng <poptako9@gmail.com>
2025-05-29 20:28:06 -0700

    Added in a level lock out but I'm not confident that it will work as restarting doesn't clear your levels and there needs to be a clear for that since the levels lock out now.  It does work however  if you clear the save data manually.

--- Assets/Code/Scripts/MISC/LevelLock.cs
+++ Assets/Code/Scripts/MISC/LevelLock.cs
@@ -4,18 +4,31 @@ using UnityEngine.UI;
 
 public class Loader : MonoBehaviour
 {
+    [SerializeField]
+    private int maxLevel = 3;
+
     void Awake()
     {
         foreach (int ID in GameManager.Instance.gameData.LevelCompleteMap.Keys)
         {
-            string trophy = "T" + ID.ToString();
-
+            int number = ID + 1;
+            string level = "L" + number.ToString();
+            if (number >= maxLevel)
+            {
+                number = maxLevel;
+                continue;
+            }
             try
             {
-                GameObject Tob = GameObject.FindGameObjectWithTag(trophy);
+                GameObject Tob = GameObject.FindGameObjectWithTag(level);
                 if (Tob != null)
                 {
                     Tob.GetComponent<Image>().enabled = true;
+                    Debug.Log("Trophy object with tag " + level + " found and enabled.");
+                }
+                else
+                {
+                    Debug.LogWarning("Trophy object with tag " + level + " not found.");
                 }
             }
             catch (UnityException)
--- Assets/Code/Scripts/UI/LevelSelect.cs
+++ Assets/Code/Scripts/UI/LevelSelect.cs
@@ -12,16 +12,16 @@ public class LevelSelect : MonoBehaviour
 
     public void OnTapDragButton()
     {
-        LevelManager.Instance.LoadLevel(2);
+        LevelManager.Instance.LoadLevel(0);
     }
 
     public void OnPinchSwipeButton()
     {
-        LevelManager.Instance.LoadLevel(3);
+        LevelManager.Instance.LoadLevel(1);
     }
 
     public void OnLightShadowButton()
     {
-        LevelManager.Instance.LoadLevel(1);
+        LevelManager.Instance.LoadLevel(2);
     }
 }
