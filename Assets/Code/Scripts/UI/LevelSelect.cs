32d34416b7393f1eb95e7bcb89322f1b7851d293
zkdavids-ucsc <zkdavids@ucsc.edu>
2025-05-31 16:14:34 -0700

    Revert Level select

--- Assets/Code/Scripts/UI/LevelSelect.cs
+++ Assets/Code/Scripts/UI/LevelSelect.cs
@@ -22,6 +22,6 @@ public class LevelSelect : MonoBehaviour
 
     public void OnLightShadowButton()
     {
-        LevelManager.Instance.LoadLevel(4);
+        LevelManager.Instance.LoadLevel(1);
     }
 }
